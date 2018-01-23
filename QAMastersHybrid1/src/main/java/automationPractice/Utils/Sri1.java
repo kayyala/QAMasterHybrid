d0d1d71db0ef709726bb52681cd6e098f4c3085d
Sudhakar Kayyala <sudhakarkayyala@gmail.com>
2018-01-23 23:10:10 +0000

    added into

---
+++ QAMastersHybrid1/src/main/java/automationPractice/Utils/Sri1.java
@@ -0,0 +1,13 @@
+package automationPractice.Utils;
+
+import org.junit.Test;
+
+/**
+ * Created by Sudhakar on 23/01/2018.
+ */
+public class Sri1 {
+    @Test
+    public void sri(){
+        System.out.println("this is testing");
+    }
+}
