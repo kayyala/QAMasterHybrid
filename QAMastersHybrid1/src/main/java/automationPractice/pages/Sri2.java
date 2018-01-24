37efd707b63d68b09b8288d70c606dc95154754a
Sudhakar Kayyala <sudhakarkayyala@gmail.com>
2018-01-24 00:39:05 +0000

    updated sri2

---
+++ QAMastersHybrid1/src/main/java/automationPractice/pages/Sri2.java
@@ -0,0 +1,10 @@
+package automationPractice.pages;
+
+/**
+ * Created by Sudhakar on 24/01/2018.
+ */
+public class Sri2 {
+    public void sri2(){
+        System.out.println("this is sri2");
+    }
+}
